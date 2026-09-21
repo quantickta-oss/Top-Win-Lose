e941243235628e5827df4fd31286b90a2c766118
quantickta-oss <quantick.ta@gmail.com>
2026-09-22 02:07:13 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,13 +1,10 @@
-// Local saving works immediately. To share reports across computers, paste your
-// live Firebase web configuration below and set enabled to true.
-// Use your existing database rules/authentication policy; do not open database rules.
 window.PL_CONFIG = {
-  enabled: false,
-  firebase: {
-    apiKey: "YOUR_FIREBASE_API_KEY",
-    authDomain: "pl-system-227d1.firebaseapp.com",
-    databaseURL: "https://pl-system-227d1-default-rtdb.firebaseio.com",
-    projectId: "pl-system-227d1",
-    appId: "1:557167025195:web:9de8f4305284ba2a045e6e"
+  "enabled": true,
+  "firebase": {
+    "apiKey": "AIzaSyB3fAsGgu7y-EHfKHOCPxdS4V-omzcEUYg",
+    "authDomain": "top-3-winners-and-losers.firebaseapp.com",
+    "databaseURL": "https://top-3-winners-and-losers-default-rtdb.firebaseio.com",
+    "projectId": "top-3-winners-and-losers",
+    "appId": "1:921488329238:web:813bdfe16621916d34c337"
   }
 };
